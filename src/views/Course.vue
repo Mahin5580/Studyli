0f04202725fc37653d261cc790a2f4fa0526a1de
Anukul Chandra <anukulchandra5585@gmail.com>
2025-08-04 00:37:18 +0600

    Esc :q! anukul
    Esc
    :q!
    
    qa
    
    vi src/views/Course.vue

--- src/views/Course.vue
+++ src/views/Course.vue
@@ -1,4 +1,5 @@
 <template>
+<<<<<<< HEAD
   <div class="courses">
     <div class="hero is-info">
       <div class="hero-body has-text-centered">
@@ -70,20 +71,99 @@
               <p>You need to have an account to continue</p>
             </template>
           </div>
+=======
+    <div class="courses">
+        <div class="hero is-info">
+            <div class="hero-body has-text-centered">
+                <h1 class="title">{{ course.title }}</h1>
+            </div>
+>>>>>>> 3b6a69d (anukul)
         </div>
-      </div>
-    </section>
-  </div>
+
+        <section class="section">
+            <div class="container">
+                <div class="columns content">
+                    <div class="column is-2">
+                        <h2>Table of contents</h2>
+
+                        <ul>
+                            <li
+                                v-for="lesson in lessons"
+                                v-bind:key="lesson.id"
+                            >
+                                <a @click="setActiveLesson(lesson)">{{ lesson.title }}</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div class="column is-10">
+                        <template v-if="$store.state.user.isAuthenticated">
+                            <template v-if="activeLesson">
+                                <h2>{{ activeLesson.title }}</h2>
+
+                                <span class="tag is-warning" v-if="activity.status == 'started'" @click="markAsDone">Started (mark as done)</span>
+                                <span class="tag is-success" v-else>Done</span>
+
+                                <hr>
+                                
+                                {{ activeLesson.long_description }}
+
+                                <hr>
+
+                                <template v-if="activeLesson.lesson_type === 'quiz'">
+                                    <Quiz
+                                        v-bind:quiz="quiz"
+                                    />
+                                </template>
+
+                                <template v-if="activeLesson.lesson_type === 'video'">
+                                    <Video
+                                        v-bind:youtube_id="activeLesson.youtube_id"
+                                    />
+                                </template>
+
+                                <template  v-if="activeLesson.lesson_type === 'article'">
+                                    <CourseComment
+                                        v-for="comment in comments"
+                                        v-bind:key="comment.id"
+                                        v-bind:comment="comment"
+                                    />
+
+                                    <AddComment
+                                        v-bind:course="course"
+                                        v-bind:activeLesson="activeLesson"
+                                        v-on:submitComment="submitComment"
+                                    />
+                                </template>
+                            </template>
+
+                            <template v-else>
+                                {{ course.long_description }}
+                            </template>
+                        </template>
+
+                        <template v-else>
+                            <h2>Restricted access</h2>
+                            
+                            <p>You need to have an account to continue!</p>
+                        </template>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from 'axios'
 
 import CourseComment from '@/components/CourseComment'
 import AddComment from '@/components/AddComment'
 import Quiz from '@/components/Quiz'
+import Video from '@/components/Video'
 
 export default {
+<<<<<<< HEAD
   components: {
     CourseComment,
     AddComment,
@@ -103,55 +183,91 @@ export default {
       quiz: {},
       
       
+=======
+    components: {
+        CourseComment,
+        AddComment,
+        Quiz,
+        Video
+    },
+    data() {
+        return {
+            course: {},
+            lessons: [],
+            comments: [],
+            activeLesson: null,
+            errors: [],
+            quiz: {},
+            activity: {}
+        }
+    },
+    async mounted() {
+        console.log('mounted')
+
+        const slug = this.$route.params.slug
+
+        await axios
+            .get(`courses/${slug}/`)
+            .then(response => {
+                console.log(response.data)
+
+                this.course = response.data.course
+                this.lessons = response.data.lessons
+            })
+        
+        document.title = this.course.title + ' | StudyNet'
+    },
+    methods: {
+        submitComment(comment) {
+            this.comments.push(comment)
+        },
+        setActiveLesson(lesson) {
+            this.activeLesson = lesson
+
+            if (lesson.lesson_type === 'quiz') {
+                this.getQuiz()
+            } else {
+                this.getComments()
+            }
+
+            this.trackStarted()
+        },
+        trackStarted() {
+            axios
+                .post(`activities/track_started/${this.$route.params.slug}/${this.activeLesson.slug}/`)
+                .then(response => {
+                    console.log(response.data)
+
+                    this.activity = response.data
+                })
+        },
+        markAsDone() {
+            axios
+                .post(`activities/mark_as_done/${this.$route.params.slug}/${this.activeLesson.slug}/`)
+                .then(response => {
+                    console.log(response.data)
+
+                    this.activity = response.data
+                })
+        },
+        getQuiz() {
+            axios
+                .get(`courses/${this.course.slug}/${this.activeLesson.slug}/get-quiz/`)
+                .then(response => {
+                    console.log(response.data)
+
+                    this.quiz = response.data
+                })
+        },
+        getComments() {
+            axios
+                .get(`courses/${this.course.slug}/${this.activeLesson.slug}/get-comments/`)
+                .then(response => {
+                    console.log(response.data)
+
+                    this.comments = response.data
+                })
+        }
+>>>>>>> 3b6a69d (anukul)
     }
-  },
-  async mounted() {
-    console.log("Courses component mounted");
-
-    const slug = this.$route.params.slug;
-    console.log(slug);
-    await axios.get(`courses/${slug}/`)
-      .then(response => {
-        console.log("Courses fetched successfully:", response.data);
-             console.log("Course slug received:", response.data.course.slug);
-        this.course = response.data.course;
-        this.lessons = response.data.lessons;
-        document.title = this.course.title + ' | Studyli';
-      })
-  },
-  methods: {
-    submitComment(comment) {
-      this.comments.push(comment)
-
-    },
-    setActiveLesson(lesson) {
-      this.activeLesson = lesson;
-      if (lesson.lesson_type === 'quiz') {
-        this.getQuiz();
-      } else {
-        this.getComments();
-      }
-    },
-    getQuiz() {
-      axios
-        .get(`courses/${this.course.slug}/${this.activeLesson.slug}/get-quiz/`)
-        .then(response => {
-          console.log(response.data);
-          this.quiz = response.data;
-        })
-    },
-    getComments() {
-        console.log("Requesting comments from:", `courses/${this.course.slug}/${this.activeLesson.slug}/get-comments/`);
-      axios
-        .get(`courses/${this.course.slug}/${this.activeLesson.slug}/get-comments/`)
-        .then(response => {
-          console.log("Comments response:", response.data);
-          console.log(response.data);
-          this.comments = response.data;
-        })
-    },
-    
-    
-  }
-}
-</script>
+}</script>
--- src/views/dashboard/MyAccount.vue
+++ src/views/dashboard/MyAccount.vue
@@ -1,14 +1,16 @@
+
 <template>
   <div class="about">
-     <div class="hero is-info ">
-    <div class="hero-body has-text-centered">
-     
+    <div class="hero is-info">
+      <div class="hero-body has-text-centered">
         <h1 class="title">My Account</h1>
       </div>
     </div>
+
     <section class="section">
       <div class="columns is-multiline">
         <div class="column is-12">
+<<<<<<< HEAD
           <h2 class="subtitle is-size-3">your active courses</h2>
         </div>
 
@@ -22,11 +24,29 @@
         </div>
       </div>
         <button @click="logout()" class="button is-danger">Log out</button>
+=======
+          <h2 class="subtitle is-size-3">Your active courses</h2>
+        </div>
+
+        <div 
+          class="column is-4"
+          v-for="course in courses"
+          v-bind:key="course.id"
+        >
+          <CourseItem :course="course" />
+        </div>
+      </div>
+
+      <hr>
+
+      <button @click="logout()" class="button is-danger">Log out</button>
+>>>>>>> 3b6a69d (anukul)
     </section>
   </div>
 </template>
 
 <script>
+<<<<<<< HEAD
 import axios from 'axios';
 import Courses from '../Courses.vue';
   import CourseItem from '@/components/CourseItem.vue';
@@ -58,12 +78,50 @@ export default {
         })
        
       axios.defaults.headers.common['Authorization'] = '';
+=======
+import axios from 'axios'
 
-            localStorage.removeItem('token');
-        this.$store.commit('removeToken');
-        this.$router.push('/');
+import CourseItem from '@/components/CourseItem.vue'
+
+export default {
+    data() {
+      return {
+        courses: []
+      }
+    },
+    components: {
+      CourseItem
+    },
+    mounted() {
+        axios
+            .get('activities/get_active_courses/')
+            .then(response => {
+                console.log(response.data)
+>>>>>>> 3b6a69d (anukul)
+
+                this.courses = response.data
+            })
+    },
+    methods: {
+        async logout() {
+            console.log('logout')
+
+            await axios
+              .post('token/logout/')
+              .then(response => {
+                console.log('Logged out')
+              })
+              .catch(error => {
+                console.log(error)
+              })
+
+            axios.defaults.headers.common['Authorization'] = ""
+
+            localStorage.removeItem('token')
+
+            this.$store.commit('removeToken')
+
+            this.$router.push('/')
+        }
     }
-  }
-}
-
-</script>
+}</script>
